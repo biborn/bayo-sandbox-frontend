dde07b8e5271968b776ee784542c8f8ecab25917
Rashid Razak <biborn@tbd.my>
2017-11-20 19:02:18 +0800

    Remove unnecessary functions in TransactionsService.

--- src/app/shared/services/transactions.service.ts
+++ src/app/shared/services/transactions.service.ts
@@ -11,48 +11,11 @@ export class TransactionsService {
     private options = new RequestOptions({ headers: this.headers });
 
     constructor(private http: Http) { }
-
-    register(user): Observable<any> {
-        return this.http.post('/api/user', JSON.stringify(user), this.options);
-    }
-
     getAllTransactions(username): Observable<any> {
         let request_body = {
             niaga_id: username
         };
         return this.http.post('http://localhost:3000/api/getalltransactions', request_body, this.options).map(res => res.json());
-    }
-
-    login(username, password): Observable<any> {
-        let credentials = {
-            username: username,
-            password: password
-        };
-        return this.http.post('http://localhost:3000/api/login', credentials, this.options);
-    }
-
-    getUsers(): Observable<any> {
-        return this.http.get('/api/users').map(res => res.json());
-    }
-
-    countUsers(): Observable<any> {
-        return this.http.get('/api/users/count').map(res => res.json());
-    }
-
-    addUser(user): Observable<any> {
-        return this.http.post('/api/user', JSON.stringify(user), this.options);
-    }
-
-    getUser(user): Observable<any> {
-        return this.http.get(`/api/user/${user._id}`).map(res => res.json());
-    }
-
-    editUser(user): Observable<any> {
-        return this.http.put(`/api/user/${user._id}`, JSON.stringify(user), this.options);
-    }
-
-    deleteUser(user): Observable<any> {
-        return this.http.delete(`/api/user/${user._id}`, this.options);
     }
 
 }
